801ccd9857bec962d73986b0c477d72e8da0d295
alraci <alraci@gmail.com>
2013-07-18 22:43:50 -0500

    Update web.js

--- web.js
+++ web.js
@@ -1,18 +1,21 @@
+var express = require('express'),
+    fs = require("fs");
 
-var express = require('express');
-var fs = require(fs);
-var app = express.createServer(express.logger());
 
-readfile = "index.html";
-var data = fs.readFileSync(readfile);
-
- buf = new buffer(data);
-
-app.get('/', function(request, response){
- response.send(buf.toString('utf8));
+var app = express.createServer();
+app.configure(function() {
+    app.use(express.logger());
+    app.use(express.static(__dirname + "/public"));
 });
+
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync("index.html").toString());
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
